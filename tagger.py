b1162f808f5d81ae97f59101957d2181ea715c4c
AyoKeito <32274835+AyoKeito@users.noreply.github.com>
2023-04-04 03:26:06 +0300

    Refactor tagger.py
    Made the code shorter. No functional differences.

--- tagger.py
+++ tagger.py
@@ -2,7 +2,7 @@
 e621 tagger
 
 Author: AyoKeito
-Version: 1.0.0
+Version: 1.0.1
 GitHub: https://github.com/AyoKeito/e621updater-python
 
 Let's tag your yiff!
@@ -23,44 +23,27 @@ def select_folder():
     return folder_selected
 
 def build_list_of_images(folder_path):
-    list_of_images = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
-            list_of_images.append(filename)
-    return list_of_images
+    return [filename for filename in os.listdir(folder_path)
+            if filename.lower().endswith((".png", ".jpg", ".jpeg"))]
 
 def calculate_md5(file_path):
-    hash_md5 = hashlib.md5()
     with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+        return hashlib.md5(f.read()).hexdigest()
 
 def search_in_posts(file_name, file_md5, posts_df):
-    if file_name in list(posts_df["md5"]):
-        index = posts_df[posts_df["md5"] == file_name].index.tolist()[0]
-        return posts_df.at[index, "tag_string"]
-    elif file_md5 in list(posts_df["md5"]):
-        index = posts_df[posts_df["md5"] == file_md5].index.tolist()[0]
-        return posts_df.at[index, "tag_string"]
+    mask = posts_df["md5"].isin([file_name, file_md5])
+    matching_rows = posts_df[mask]
+    if len(matching_rows) > 0:
+        return matching_rows.iloc[0]["tag_string"]
     else:
-        print("File:", file_name, " not found in the posts data")
         return None
 
 def write_to_exif(file_path, tag_string):
     artists_df = pd.read_parquet("artists.parquet")
-    artists_list = artists_df['name'].tolist()
-    subject_tags = []
-    creator_tags = []
-    tags = tag_string.split(", ")
-    trash_tags = ["conditional_dnp", "avoid_posting", "unknown_artist", "absurd_res", "hi_res", "digital_media_(artwork)", "traditional_media_(artwork)"]
-    for tag in tags:
-        if tag in artists_list:
-            creator_tags.append(tag)
-        elif tag not in trash_tags:
-            subject_tags.append(tag)
-    subject_tags = ", ".join(subject_tags)
-    creator_tags = ", ".join(creator_tags)
+    artists_list = set(artists_df['name'])
+    trash_tags = {"conditional_dnp", "avoid_posting", "unknown_artist", "absurd_res", "hi_res", "digital_media_(artwork)", "traditional_media_(artwork)"}
+    subject_tags = ", ".join(tag for tag in tag_string.split(", ") if tag not in trash_tags and tag not in artists_list)
+    creator_tags = ", ".join(tag for tag in tag_string.split(", ") if tag in artists_list)
     with exiftool.ExifTool() as et:
         et.execute(f"-xmp-dc:subject={subject_tags}", "-overwrite_original_in_place", file_path)
         et.execute(f"-xmp-dc:creator={creator_tags}", "-overwrite_original_in_place", file_path)
@@ -79,7 +62,6 @@ args = parser.parse_args()
 
 if not args.in_file and not args.in_txt:
     parser.error("At least one of the arguments --in-file (-f) or --in-txt (-t) must be specified")
-
 if args.folder_path:
     folder_path = args.folder_path
 else:
@@ -87,65 +69,48 @@ else:
 
 list_of_images = build_list_of_images(folder_path)
 posts_df = pd.read_parquet("posts.parquet")
-
 processed_count = 0
 not_found = []
 renamed_files = []
-
 total_files = len(list_of_images)
 
 print("{:<6} {:<36} {:<14} {:<30}".format("%", "Name", "Found/Missing", "Tags"))
 for image_file in list_of_images:
     file_path = os.path.join(folder_path, image_file)
     file_name_without_extension, file_extension = os.path.splitext(image_file)
-    tag_string = search_in_posts(file_name_without_extension, None, posts_df)
-    found_by = "Found (NAME)"
-    if tag_string is None:
-        file_md5 = calculate_md5(file_path)
-        tag_string = search_in_posts(None, file_md5, posts_df)
-        if tag_string is not None:
-            found_by = "Found (MD5)"
-            tag_string = tag_string.replace(" ", ", ")
-        else:
-            tag_string = "MISSING"
-            found_by = "MISSING"
+    tag_string, found_by = "MISSING", "MISSING"
+    if (tag_string := search_in_posts(file_name_without_extension, None, posts_df)) is not None:
+        tag_string, found_by = tag_string.replace(" ", ", "), "Found (NAME)"
     else:
-        found_by = "Found (NAME)"
-        tag_string = tag_string.replace(" ", ", ")
+        if (tag_string := search_in_posts(None, calculate_md5(file_path), posts_df)) is not None:
+            tag_string, found_by = tag_string.replace(" ", ", "), "Found (MD5)"
     if args.in_file:
         write_to_exif(file_path, tag_string)
     if args.in_txt:
         write_to_txt(file_path, tag_string)
     if found_by == "MISSING":
-        print("\033[31m{:<6} {:<36} {:<14} {:<30}\033[0m".format(f"{(processed_count/total_files)*100:.2f}%", image_file, found_by, tag_string[:30]))
         not_found.append(file_path)
-    if found_by == "Found (MD5)":
-        if args.no_rename:
-            print("\033[33m{:<6} {:<36} {:<14} {:<30}\033[0m".format(f"{(processed_count/total_files)*100:.2f}%", image_file, found_by, tag_string[:30]))
-        else:
-            print("\033[33m{:<6} {:<36} {:<14} {:<30}\033[0m".format(f"{(processed_count/total_files)*100:.2f}%", image_file, found_by, tag_string[:30]))
-            new_file_name = f"{file_md5}{file_extension}"
+    elif found_by == "Found (MD5)":
+        new_file_name = f"{calculate_md5(file_path)}{file_extension}"
+        if not args.no_rename:
             os.rename(file_path, os.path.join(folder_path, new_file_name))
             renamed_files.append((image_file, new_file_name))
-    if found_by == "Found (NAME)":
-        print("\033[32m{:<6} {:<36} {:<14} {:<30}\033[0m".format(f"{(processed_count/total_files)*100:.2f}%", image_file, found_by, tag_string[:30]))
+    print(f"\033[{31 if found_by == 'MISSING' else 33 if found_by == 'Found (MD5)' else 32}m{f'{(processed_count/total_files)*100:.2f}%':<6} {image_file:<36} {found_by:<14} {tag_string[:30]:<30}\033[0m")
     processed_count += 1
 
-if len(not_found) == 0:
-    print("Processed image", processed_count, "of", len(list_of_images), "\n", "All files are found")
-else:
-    print("Processed image", processed_count, "of", len(list_of_images), "\n", len(not_found), "files are not found")
-
-if len(not_found) > 0:
+if not_found:
     not_found_directory = os.path.join(folder_path, "NotFound")
     os.makedirs(not_found_directory, exist_ok=True)
     for file_path in not_found:
         destination_path = os.path.join(not_found_directory, os.path.basename(file_path))
         shutil.move(file_path, not_found_directory)
+    print("Processed image", processed_count, "of", len(list_of_images), "\n", len(not_found), "files are not found")
+else:
+    print("Processed image", processed_count, "of", len(list_of_images), "\n", "All files are found")
 
 print("Finished processing all images.")
 
-if len(renamed_files) > 0:
+if renamed_files:
     with open(os.path.join(folder_path, "_renamed_files.txt"), "w") as file:
         for original_name, new_name in renamed_files:
             file.write(f"{original_name} renamed to {new_name}\n")
